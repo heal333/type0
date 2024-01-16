744e59efa453a5e1c1fcc4de0d7cdf6fc6549c78
heal333 <rohanaich333@gmail.com>
2024-01-17 04:50:13 +0530

    fixing the global high score logic in previous version the it used to check only from the local storage

--- scrpt.js
+++ scrpt.js
@@ -9,37 +9,31 @@ const accuracy = document.querySelector(".accuracy");
 const localhighest = document.querySelector(".highest");
 const globalHighest = document.querySelector(".globalHighest");
 const root = document.querySelector(":root");
-let highestS;
+let highestScoreLocal;
 let isWhite = false;
 let iniTime;
 let speed;
 let wrongHit = 0;
+let highestScoreGlobal;
 
 const randStream = (Math.random() * 5).toFixed();
 console.log(randStream);
-
 inp.focus(); //for focusing on the input section without clicking explicitly
 
-//for setting the highest score from local storage
-if (window.localStorage.highest) {
-  highestS = window.localStorage.highest;
-  localhighest.innerText = `highest: ${highestS} WPM`;
-} else {
-  highestS = 0;
-  window.localStorage.setItem("highest", 0);
-}
 
 
 //to render the paragraph
 function streamRender(streams) {
-  let wordEliment ="";
+  let wordEliment = "";
   for (let i = 0; i < streams.length; i++) {
     let temp = `<div class="ltr${i} letters">${streams[i]}</div>`;
-    wordEliment +=temp;
-    if (streams[i]==" "){
-      textStream.insertAdjacentHTML("beforeend", `<div class="word">${wordEliment}</div>`);
+    wordEliment += temp;
+    if (streams[i] == " ") {
+      textStream.insertAdjacentHTML(
+        "beforeend",
+        `<div class="word">${wordEliment}</div>`
+      );
       wordEliment = "";
-
     }
   }
 }
@@ -49,51 +43,74 @@ function inputChecker(streams) {
   letter = 0;
   inp.addEventListener("input", (event) => {
     if (event.target.value === streams[letter]) {
-      document.querySelector(`.ltr${letter}`).style.color = "gray";
+      // document.querySelector(`.ltr${letter}`).style.color = "gray";
+      document.querySelector(`.ltr${letter}`).classList.add("visitedLetters");
       letter++;
       if (!iniTime) {
         iniTime = new Date();
       }
     } else {
       wrongHit += 1;
+      // document.querySelector(`.ltr${letter}`).style.backgroundColor = "white";
+      // letter++;
     }
     speed = ((letter / ((new Date() - iniTime) / 1000)) * 60) / 5; // this will calculate the speed in WPM
     errorPercentage = (letter / (letter + wrongHit)) * 100; // to calculate the accuracy
 
-    if ((letter > 333) & (highestS < speed)) {
-      //to determine if the current typing speed is the highsest speed
-      highestS = speed.toFixed();
-      window.localStorage.setItem("highest", highestS);
+    //to determine if the current typing speed is the highsest speed 
+    if (letter > 333){
+      if ((highestScoreLocal < speed)){
+      highestScoreLocal = speed.toFixed();
+      window.localStorage.setItem("highest", highestScoreLocal);
+      }
+      if (speed>highestScoreGlobal){
+        setGlobalHigh(speed.toFixed())
+      }
     }
 
+    
     //for rendering the the speed, accuracy and highest score
     accuracy.innerText = `accuracy: ${errorPercentage.toFixed(2)}%`;
     bench.innerText = `Speed: ${speed.toFixed()} WPM`;
-    localhighest.innerText = `highest: ${highestS} WPM`;
+    localhighest.innerText = `highest: ${highestScoreLocal} WPM`;
     event.target.value = "";
   });
 }
 
-const setGlobalHigh = (speed)=>{
+//sends global highscore to the backend
+const setGlobalHigh = (speed) => {
   fetch(
     "https://type0-a8335-default-rtdb.asia-southeast1.firebasedatabase.app/stats.json",
     { method: "PUT", body: JSON.stringify({ globalHigh: speed }) }
   );
+  globalHighest.innerText = `globalHighest: ${speed} WPM`;
+};
+
+
+
+
+
+//for setting the highest score from local storage
+if (window.localStorage.highest) {
+  highestScoreLocal = window.localStorage.highest;
+  localhighest.innerText = `highest: ${highestScoreLocal} WPM`;
+} else {
+  highestScoreLocal = 0;
+  window.localStorage.setItem("highest", 0);
 }
 
-
-
+//for getting the global highscore form the backend
 fetch(
   "https://type0-a8335-default-rtdb.asia-southeast1.firebasedatabase.app/stats/globalHigh.json"
 )
   .then((response) => response.json())
   .then((response) => {
-    if (response<highestS){
-      setGlobalHigh(highestS)
-      response = highestS
-    }
+    highestScoreGlobal=response
     globalHighest.innerText = `globalHighest: ${response} WPM`;
   });
+
+
+//calls previously defined funtions to render texts and check inputs
 fetch(
   `https://type0-a8335-default-rtdb.asia-southeast1.firebasedatabase.app/txtStreams/stream00${randStream}.json`
 )
@@ -104,17 +121,20 @@ fetch(
     inputChecker(streams);
   });
 
+
 //implement theme change when double clicking
 html.addEventListener("dblclick", () => {
   if (isWhite) {
     root.style.setProperty("--background", "black");
     root.style.setProperty("--statsBackground", "black");
-    root.style.setProperty("--fontColor", "white");
+    root.style.setProperty("--visited", "white");
     isWhite = false;
   } else {
     root.style.setProperty("--background", "white");
     root.style.setProperty("--statsBackground", "white");
-    root.style.setProperty("--fontColor", "black");
+    root.style.setProperty("--visited", "black");
     isWhite = true;
   }
 });
+
+//setGlobalHigh(0); // to reset the global highscore in the backend
